11d2b70739d2c13edfb25ef32141cc3542a32ec6
codethi <docencia.thiago@gmail.com>
2021-08-23 21:42:57 -0300

    array to object

--- index.js
+++ index.js
@@ -4,24 +4,33 @@ const app = express(); // cria o nosso objeto app, que vai poder utilizar tudo o
 
 app.use(express.json()); // Converte requisições e repostas para JSON (JavaScript Object Notation)
 
-const port = 3000 // constante para salvar a porta do servidor;
+const port = 3000; // constante para salvar a porta do servidor;
 
 const filmes = [
-    "Capitão America", 
-    "Capitã Marvel", 
-    "Pantera Negra"
+  {
+    id: 1,
+    nome: "Capitão America",
+    duracao: 160,
+  },
+  {
+    id: 2,
+    nome: "Capitã Marvel",
+    duracao: 200,
+  },
 ];
+
 
 // CRUD - Create[POST] - Read[GET] - Update[PUT] - Delete[DELETE]
 
 // GET / - home
-app.get("/", (req, res) => { // rota de GET, recebe o nome da rota e uma função de callback com requisição ao servidor e resposta do servidor.
+app.get("/", (req, res) => {
+  // rota de GET, recebe o nome da rota e uma função de callback com requisição ao servidor e resposta do servidor.
   res.status(200).send("Hello World Express"); // Responde na tela um texto.
 });
 
 // GET /filmes - Retornar a lista de filmes
 app.get("/filmes", (req, res) => {
-  res.json({filmes});
+  res.json({ filmes }); // .json converte nosso array ou objeto para JSON
 });
 
 // GET /filmes/{id} - Retornar a lista de filmes pelo ID
@@ -43,22 +52,21 @@ app.post("/filmes", (req, res) => {
 app.put("/filmes/:id", (req, res) => {
   const id = req.params.id - 1;
   const filme = req.body.filme;
-  filmes[id] = filme
+  filmes[id] = filme;
   res.send("Filme alterado com sucesso!");
 });
 
 // Delete - filmes/{id} - apagar um filme pelo ID
 app.delete("/filmes/:id", (req, res) => {
-    const id = req.params.id - 1;
-    delete filmes[id]
-    res.send("Filme apagado com sucesso!");
-})
-
-
+  const id = req.params.id - 1;
+  delete filmes[id];
+  res.send("Filme apagado com sucesso!");
+});
 
 /* 
 A função listen do objeto app serve para "ligar" o nosso back-end ou servir o nosso back-end
 É obrigatório que essa chamada de função esteja SEMPRE no final do nosso código! */
-app.listen(port, () => { // recebe dois parametros, a porta e um função de callback para principalmente mostra um mensagem no console.
-  console.log(`Servidor rodando em http://localhost:${port}`)
+app.listen(port, () => {
+  // recebe dois parametros, a porta e um função de callback para principalmente mostra um mensagem no console.
+  console.log(`Servidor rodando em http://localhost:${port}`);
 });
