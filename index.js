6df1cf51b25594cbb3656d9d1be70f2da83c9b77
violigon <vinicius@oligon.com.br>
2021-08-30 20:27:23 -0300

    new index

--- index.js
+++ index.js
@@ -6,12 +6,29 @@ const ObjectId = mongodb.ObjectId;
 	const app = express();
 	app.use(express.json());
 	const port = 3000;
+	const connectionString = `mongodb://localhost:27017/blue_db`;
+
+	const options = {
+		useUnifiedTopology: true,
+	};
+
+	const client = await mongodb.MongoClient.connect(connectionString, options);
+
+	const db = client.db("blue_db");
+	const personagens = db.collection("personagens");
+
+	const getPersonagensValidas = () => personagens.find({}.toArray());
+
+	const getPersonagemById = async (id) =>
+		personagens.findOne({ _id: ObjectId(id) });
 
 	app.get("/", (req, res) => {
-		res.send({ "info": "Olá, Blue" });
+		res.send({ info: "Olá, Blue" });
 	});
 
-  
+	app.get("/personagens", async (req, res) => {
+		res.send(await getPersonagensValidas());
+	});
 
 	app.listen(port, () => {
 		console.info(`App rodando em http://localhost:${port}`);
