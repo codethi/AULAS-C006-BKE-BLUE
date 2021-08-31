78277a94fee58d41f4f35719674f8b5c9ce71121
violigon <vinicius@oligon.com.br>
2021-08-30 23:02:59 -0300

    ta comitado

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const mongodb = require("mongodb");
-//const ObjectId = mongodb.ObjectId;
-require ("dotenv").config();
+const ObjectId = mongodb.ObjectId;
+require("dotenv").config();
 
 (async () => {
-    const dbHost = process.env.DB_HOST;
-    const dbPort = process.env.DB_PORT;
-    const dbName = process.env.DB_NAME;
-
+	const dbHost = process.env.DB_HOST;
+	const dbPort = process.env.DB_PORT;
+	const dbName = process.env.DB_NAME;
 
 	const app = express();
 	app.use(express.json());
@@ -23,19 +22,84 @@ require ("dotenv").config();
 	const db = client.db("blue_db");
 	const personagens = db.collection("personagens");
 
-    const getPersonagensValidas = () => personagens.find({}).toArray();
+	const getPersonagensValidas = () => personagens.find({}).toArray();
 
-  // const getPersonagemById = async(id) => personagens.findOne({_id: ObjectId(id)});
+	const getPersonagemById = async (id) =>
+		personagens.findOne({ _id: ObjectId(id) });
+
+	app.all("/*", (req, res, next) => {
+		res.header("Access-Control-Allow-Origin", "*");
+
+		res.header("Access-Control-Allow-Methods", "*");
+
+		res.header(
+			"Access-Control-Allow-Headers",
+			"Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization"
+		);
+
+		next();
+	});
 
 	app.get("/", (req, res) => {
 		res.send({ info: "Olá, Blue" });
 	});
 
-    //[GET] GetAllPersonagens
+	//[GET] GetAllPersonagens
 
-    app.get("/personagens", async (req, res) => {
-        res.send(await getPersonagensValidas())
-    });
+	app.get("/personagens", async (req, res) => {
+		res.send(await getPersonagensValidas());
+	});
+
+	//[GET] getPersonagemById
+
+	app.get("/personagens/:id", async (req, res) => {
+		const id = req.params.id;
+		const personagem = await getPersonagemById(id);
+		res.send(personagem);
+	});
+
+	app.post("/personagens", async (req, res) => {
+		const objeto = req.body;
+
+		if (!objeto || !objeto.nome || !objeto.imagemUrl) {
+			res.send("Objeto invalido");
+			return;
+		}
+
+		const { insertCount } = await personagens.insertOne(objeto);
+
+		if (insertCount !== 1) {
+			res.send("Ocorreu um erro");
+			return;
+		}
+
+		res.send(objeto);
+	});
+
+	app.put("/personagens/:id", async (req, res) => {
+		const id = req.params.id;
+		const objeto = req.body;
+		res.send(
+			await personagens.updateOne(
+				{
+					_id: ObjectId(id),
+				},
+				{
+					$set: objeto,
+				}
+			)
+		);
+	});
+
+	app.delete("/personagens/:id", async (req, res) => {
+		const id = req.params.id;
+
+		res.send(
+			await personagens.deleteOne({
+				_id: ObjectId(id),
+			})
+		);
+	});
 
 	app.listen(port, () => {
 		console.info(`App rodando em http://localhost:${port}`);
